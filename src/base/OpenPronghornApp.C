14d255dc6e7126e08ca28bfcb273c1be98d6946a
Logan Harbour <loganhharbour@gmail.com>
2025-01-22 13:07:38 -0700

    Initial files

---
+++ src/base/OpenPronghornApp.C
@@ -0,0 +1,51 @@
+#include "OpenPronghornApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+OpenPronghornApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  params.set<bool>("use_legacy_initial_residual_evaluation_behavior") = false;
+  return params;
+}
+
+OpenPronghornApp::OpenPronghornApp(InputParameters parameters) : MooseApp(parameters)
+{
+  OpenPronghornApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+OpenPronghornApp::~OpenPronghornApp() {}
+
+void
+OpenPronghornApp::registerAll(Factory & f, ActionFactory & af, Syntax & syntax)
+{
+  ModulesApp::registerAllObjects<OpenPronghornApp>(f, af, syntax);
+  Registry::registerObjectsTo(f, {"OpenPronghornApp"});
+  Registry::registerActionsTo(af, {"OpenPronghornApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+OpenPronghornApp::registerApps()
+{
+  registerApp(OpenPronghornApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+OpenPronghornApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  OpenPronghornApp::registerAll(f, af, s);
+}
+extern "C" void
+OpenPronghornApp__registerApps()
+{
+  OpenPronghornApp::registerApps();
+}
---
+++ test/src/base/OpenPronghornTestApp.C
@@ -0,0 +1,63 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+#include "OpenPronghornTestApp.h"
+#include "OpenPronghornApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+
+InputParameters
+OpenPronghornTestApp::validParams()
+{
+  InputParameters params = OpenPronghornApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  params.set<bool>("use_legacy_initial_residual_evaluation_behavior") = false;
+  return params;
+}
+
+OpenPronghornTestApp::OpenPronghornTestApp(InputParameters parameters) : MooseApp(parameters)
+{
+  OpenPronghornTestApp::registerAll(
+      _factory, _action_factory, _syntax, getParam<bool>("allow_test_objects"));
+}
+
+OpenPronghornTestApp::~OpenPronghornTestApp() {}
+
+void
+OpenPronghornTestApp::registerAll(Factory & f, ActionFactory & af, Syntax & s, bool use_test_objs)
+{
+  OpenPronghornApp::registerAll(f, af, s);
+  if (use_test_objs)
+  {
+    Registry::registerObjectsTo(f, {"OpenPronghornTestApp"});
+    Registry::registerActionsTo(af, {"OpenPronghornTestApp"});
+  }
+}
+
+void
+OpenPronghornTestApp::registerApps()
+{
+  registerApp(OpenPronghornApp);
+  registerApp(OpenPronghornTestApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+// External entry point for dynamic application loading
+extern "C" void
+OpenPronghornTestApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  OpenPronghornTestApp::registerAll(f, af, s);
+}
+extern "C" void
+OpenPronghornTestApp__registerApps()
+{
+  OpenPronghornTestApp::registerApps();
+}
